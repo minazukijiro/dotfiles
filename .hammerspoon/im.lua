e69f25938447e9e42e1ee4e0a55d2355b3d33a73
ﾀﾅｶ ﾄﾓｱｷ <minazukijiro@gmail.com>
2024-11-06 10:37:54 +0900

    update .hammerspoon/im.lua

--- .hammerspoon/im.lua
+++ .hammerspoon/im.lua
@@ -1,28 +1,46 @@
--- im = require 'im'
--- im.applications = {
---    kitty = 'en',
---    Slack = 'ja',
--- }
+-- local IM = require 'im'
+
+-- local im = IM.new({
+--       'Emacs' = 'en',
+--       'Kitty' = 'en',
+--       'Slack' = 'ja'
+-- })
+
+-- im.watcher:start()
 
 local application  = require('hs.application')
 local watcher = application.watcher
 local eventtap = require('hs.eventtap')
 local keystroke = eventtap.keyStroke
 
-local module = {}
-module.applications = {}
+local inputmethod = {}
+inputmethod['en'] = 0x66
+inputmethod['ja'] = 0x68
 
 local im = {}
-im['en'] = 0x66
-im['ja'] = 0x68
 
-module.applicationWatcher = watcher.new(function(appName, eventType, appObject)
-      for k,v in pairs(module.applications) do
-         if (appName == k and eventType == watcher.activated) then
-            keystroke({}, im[v], 0)
-            break
+im.new = function(settings)
+   local obj = {}
+
+   obj.watcher = watcher.new(function(appName, eventType, appObject)
+         for k, v in pairs(settings) do
+            if (appName == k and eventType == watcher.activated) then
+               keystroke({}, inputmethod[v], 0)
+               break
+            end
          end
-      end
-end):start()
+   end)
 
-return module
+   return obj
+end
+
+return im
+
+-- module.applicationWatcher = watcher.new(function(appName, eventType, appObject)
+--       for k,v in pairs(module.applications) do
+--          if (appName == k and eventType == watcher.activated) then
+--             keystroke({}, im[v], 0)
+--             break
+--          end
+--       end
+-- end):start()
